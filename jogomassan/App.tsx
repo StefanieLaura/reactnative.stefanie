e8a093de56080e6878c21b93342bd7af01890a6d
Álcool145 <160517752+jelf2006@users.noreply.github.com>
2024-09-02 09:55:18 -0300

    Modificação dia 02 do 09

--- jogomassan/App.tsx
+++ jogomassan/App.tsx
@@ -5,7 +5,7 @@ import {View, Text} from 'react-native';
 export default function App() {
   return(
     <View>
-    <Text> Helo Word, está na hora de Codar </Text>
+    <Text> Helo Word, o professor massan é toptoptop top, ele precisa cantar pra nós </Text>
     </View>
   );
 }
